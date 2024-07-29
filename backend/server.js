c96e23602f375a69547653e1356814760df34581
Neelesh <neeleshsingh2072003@gmail.com>
2024-07-29 23:32:06 +0530

    Suggested Users Fetched Added

--- backend/server.js
+++ backend/server.js
@@ -29,7 +29,7 @@ const app = express();
 const PORT = process.env.PORT || 5000
 
 
-app.use(express.json()); // to parse req.body
+app.use(express.json({limit:"5mb"})); // to parse req.body
 app.use(express.urlencoded({ extended: true })); //To parse form data(urlencoded)
 app.use(cookieParser());
 
